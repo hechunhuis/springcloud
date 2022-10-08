9ebc0fef095dac7c88516d5f06b5738acd1bf4b4
tomato <hechunhui_email@163.com>
2022-10-08 23:02:55 +0800

    refactor 生产者-支付模块注册到服务中心

--- cloud-provider-payment-8001/src/main/java/com/tomato/springcloud/PaymentMain.java
+++ cloud-provider-payment-8001/src/main/java/com/tomato/springcloud/PaymentMain.java
@@ -2,14 +2,16 @@ package com.tomato.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author : tomato<hechunhui_email@163.com>
  * @date : 2022/10/7 21:58
  * @className : PaymentMain
- * @description: TODO
+ * @description: 支付模块的主启动类
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain.class, args);
