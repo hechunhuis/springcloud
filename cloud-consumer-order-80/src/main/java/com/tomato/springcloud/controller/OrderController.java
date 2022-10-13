2af540374552dae45327792321071e45d8a32c40
何春辉 <hechunhui_email@163.com>
2022-10-13 18:56:17 +0800

    refactor Ribbon restTemplate 的 ForEntity使用

--- cloud-consumer-order-80/src/main/java/com/tomato/springcloud/controller/OrderController.java
+++ cloud-consumer-order-80/src/main/java/com/tomato/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.tomato.springcloud.commons.CommonResult;
 import com.tomato.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,15 +28,57 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
+    /**
+     * postForObject
+     * @param payment
+     * @return
+     */
     @PostMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment){
+    public CommonResult<Payment> create(Payment payment) {
         log.info("调用：/consumer/payment/create");
         return restTemplate.postForObject(PAYMENT_NAME + "/payment/create", payment, CommonResult.class);
     }
 
+    /**
+     * getForObject
+     * @param id
+     * @return
+     */
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         log.info("调用：/consumer/payment/get/{id}");
         return restTemplate.getForObject(PAYMENT_NAME + "/payment/get/" + id, CommonResult.class);
+    }
+
+    /**
+     * postForEntity
+     * @param payment
+     * @return
+     */
+    @PostMapping("/consumer/payment/createForEntity")
+    public CommonResult<Payment> createForEntity(Payment payment) {
+        log.info("调用：/consumer/payment/create");
+        ResponseEntity<CommonResult> responseEntity = restTemplate.postForEntity(PAYMENT_NAME + "/payment/create", payment, CommonResult.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            return new CommonResult(444, "操作失败");
+        }
+    }
+
+    /**
+     * getForEntity
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPaymentForEntity(@PathVariable("id") Long id) {
+        log.info("调用：/consumer/payment/getForEntity/{id}");
+        ResponseEntity<CommonResult> responseEntity = restTemplate.getForEntity(PAYMENT_NAME + "/payment/get/" + id, CommonResult.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            return new CommonResult(444, "操作失败");
+        }
     }
 }
--- cloud-provider-payment-zk-8004/src/main/java/com/tomato/springcloud/PaymentMainZK8004.java
+++ cloud-provider-payment-zk-8004/src/main/java/com/tomato/springcloud/PaymentMainZK8004.java
@@ -12,8 +12,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentMain8004 {
+public class PaymentMainZK8004 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8004.class, args);
+        SpringApplication.run(PaymentMainZK8004.class, args);
     }
 }
